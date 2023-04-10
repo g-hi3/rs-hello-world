aa99dd55041ea2276578d85d1bee5e613bd74c87
G_hi3 <marc.forrer@bluewin.ch>
2023-04-10 22:11:22 +0200

    fixed build errors for iterator-performance.rs

--- src/iterator-performance.rs
+++ src/iterator-performance.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let buffer: &mut [i32];
-    let coefficients: [i64; 12];
-    let qlp_shift: i16;
+    // The example leaves these values empty, but it doesn't compile that way, so I added some.
+    let buffer: &mut [i32] = &mut [32, 16, 8, 4, 2, 1];
+    let coefficients: [i64; 12] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let qlp_shift: i16 = 96;
 
     // The Rust compiler "unrolls" this loop, because the number of iterations is known at compile time.
     // This means, after compilation, there will be no loop left and the code is executed 12 times.
