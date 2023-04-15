a8e193dbe93ad801d4fbc8d947a5ab1595e5608d
Marc Forrer <marc.forrer@bluewin.ch>
2023-04-15 15:31:39 +0200

    finished chapter 15.5

--- src/smart-pointers.rs
+++ src/smart-pointers.rs
@@ -80,6 +80,19 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    // Interior mutability is a design pattern in Rust that allows mutation of immutable data structures.
+    // We use unsafe code to do this.
+    // Unsafe code tells the compiler that we check the borrowing rules manually, instead of relying on the compiler.
+
+    // `RefCell<T>` enforces borrowing rules at runtime instead of compile time.
+    // If the borrowing rules are broken at runtime, the program panics.
+    // `RefCell<T>` is intended for single threaded code (`Mutex<T>` for multithreaded code).
+    // It counts the references provided by `borrow()` and `borrow_mut()`.
+    // The borrowing rules are enforces like this at runtime.
+    // That means, calling `borrow_mut()` twice will cause it to panic.
+
+    // Using `Rc<RefCell<T>>` allows you to have multiple mutable ownership over a value.
 }
 
 fn hello(name: &str) {
@@ -121,5 +134,87 @@ struct CustomSmartPointer {
 impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
         println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger
+                .send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // With `borrow_mut()`, we get a mutable reference to the vector.
+            // We can add the String here.
+            // It returns a `RefMut<T>` that implements `Deref`.
+            self.sent_messages
+                .borrow_mut()
+                .push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        // With `borrow()`, we get an immutable reference of the vector.
+        // We can use it to check the state and not modify the vector.
+        // It returns `Ref<T>` which implements `Deref`.
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
